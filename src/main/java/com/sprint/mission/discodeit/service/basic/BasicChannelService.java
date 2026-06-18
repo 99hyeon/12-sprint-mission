64001b83eef8a00afe915aae367021ccedf55c93
99hyeon <hyeon99@ajou.ac.kr>
2026-06-18 14:10:27 +0900

    refactor: 코ë드리뷰 반영

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -25,6 +25,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -118,6 +119,7 @@ public class BasicChannelService implements ChannelService {
   }
 
   @Override
+  @Transactional
   public void delete(UUID id) {
     getChannelOrThrow(id);
 
